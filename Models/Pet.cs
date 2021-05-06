68ce7718f830101242a8fbaf4fb1589ae4c2096a
Bianca Brown <biancakbrown58@gmail.com>
2021-05-06 13:51:45 -0400

    added tables pet, feeding, playtime and scolding

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace TamagotchiiAPI.Models
 {
@@ -6,8 +7,12 @@ namespace TamagotchiiAPI.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime Birthday { get; set; }
+        public DateTime Birthday { get; set; } = DateTime.Now;
         public int HungerLevel { get; set; }
         public int HappinessLevel { get; set; }
+
+        public List<Playtime> Playtimes { get; set; }
+        public List<Feeding> Feedings { get; set; }
+        public List<Scolding> Scoldings { get; set; }
     }
 }
---
+++ Models/Playtime.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TamagotchiiAPI.Models
+{
+    public class Playtime
+    {
+        public int Id { get; set; }
+        public DateTime When { get; set; } = DateTime.Now;
+        public int PetId { get; set; }
+        public Pet Pet { get; set; }
+
+    }
+}
